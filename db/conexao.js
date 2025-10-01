fa1985e21643a1a2c4b7a5f9fc4d5321efbb6f67
felpzw <lipewinsch@gmail.com>
2025-09-30 22:31:54 -0300

    Port Error Solved

--- db/conexao.js
+++ db/conexao.js
@@ -1,20 +1,10 @@
 const mongoose = require("mongoose");
 
-
-const MONGODB_URI = "mongodb://root:root@localhost:27017/PETSHOP?authSource=admin";
-
-mongoose.set('strictQuery', true);
-
 mongoose
-  .connect(MONGODB_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
-  .then(() => console.log("Conectado ao MongoDB: PETSHOP"))
-  .catch((err) => {
-    console.error("Erro ao conectar ao MongoDB:", err.message);
-    process.exit(1);
-  });
+  .connect("mongodb://localhost:27017/PETSHOP", {})
+  .then(() => console.log("Conectado ao MongoDB:PETSHOP"))
+  .catch((err) => console.error("Erro ao conectar:", err));
 
 module.exports = mongoose;
+
 
